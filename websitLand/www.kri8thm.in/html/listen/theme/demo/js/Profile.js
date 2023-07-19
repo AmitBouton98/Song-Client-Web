3d3a7f0c3f8d1a1f81df58f9f33d40e3a03ac663
AmitBouton98 <118891774+AmitBouton98@users.noreply.github.com>
2023-07-19 12:03:24 +0300

    fix profile src script by amit

--- websitLand/www.kri8thm.in/html/listen/theme/demo/js/Profile.js
+++ websitLand/www.kri8thm.in/html/listen/theme/demo/js/Profile.js
@@ -1,48 +1,49 @@
-function LoadProfile(){
-    var data = JSON.parse(sessionStorage.getItem('User'));
-    LoadProfileDetails(data)
+function LoadProfile() {
+  var data = JSON.parse(sessionStorage.getItem('User'));
+  LoadProfileDetails(data)
 }
 
-function LoadProfileDetails(data){ 
+function LoadProfileDetails(data) {
 
-    let FirstNameUser = document.getElementById("FirstNameUser")
-    FirstNameUser.value = data.first
-    let LastNameUser = document.getElementById("LastNameUser")
-    LastNameUser.value = data.last
-    let EmailUser = document.getElementById("EmailUser")
-    EmailUser.value = data.email
-    let SaveProfileDetails = document.getElementById("SaveProfileDetails")
-    SaveProfileDetails.onclick = ()=>{
-        UpdateUserDetails((item)=>{
-            console.log(item)
-        },document.getElementById("FirstNameUser").value,document.getElementById("LastNameUser").value,document.getElementById("EmailUser").value,data.id)
-    }
-    let ChangePasswordUser = document.getElementById("ChangePasswordUser")
-    ChangePasswordUser.onclick = ()=>{
-        // show new window for update password with button save
-        SwalChangePassword(data)
-        // create procedure for it
-    }
+  let FirstNameUser = document.getElementById("FirstNameUser")
+  FirstNameUser.value = data.first
+  let LastNameUser = document.getElementById("LastNameUser")
+  LastNameUser.value = data.last
+  let EmailUser = document.getElementById("EmailUser")
+  EmailUser.value = data.email
+  let SaveProfileDetails = document.getElementById("SaveProfileDetails")
+  SaveProfileDetails.onclick = () => {
+    UpdateUserDetails((item) => {
+      console.log(item)
+    }, document.getElementById("FirstNameUser").value, document.getElementById("LastNameUser").value, document.getElementById("EmailUser").value, data.id)
+  }
+  let ChangePasswordUser = document.getElementById("ChangePasswordUser")
+  ChangePasswordUser.onclick = () => {
+    // show new window for update password with button save
+    SwalChangePassword(data)
+    // create procedure for it
+  }
 }
-function SwalChangePassword(data){
-    Swal.fire({
-        title: 'Change password',
-        html: `<input type="password" id="OldPassowrd" class="swal2-input" placeholder="Old password">
+function SwalChangePassword(data) {
+  Swal.fire({
+    title: 'Change password',
+    html: `<input type="password" id="OldPassowrd" class="swal2-input" placeholder="Old password">
         <input type="password" id="NewPassword" class="swal2-input" placeholder="New password">`,
-        confirmButtonText: 'Save changes',
-        focusConfirm: false,
-        preConfirm: () => {
-          const OldPassowrd = Swal.getPopup().querySelector('#OldPassowrd').value
-          const NewPassword = Swal.getPopup().querySelector('#NewPassword').value
-          if (!OldPassowrd || !NewPassword) {
-            Swal.showValidationMessage(`Please enter login and password`)
-          }
-          return { OldPassowrd: OldPassowrd, NewPassword: NewPassword }
-        }
-      }).then((result) => {
-        ChangePassowrdForUser((data)=>{
-            console.log(data)
-        },data.id,result.value.OldPassowrd,result.value.NewPassword)
-      })
-      
+    confirmButtonText: 'Save changes',
+    focusConfirm: false,
+    preConfirm: () => {
+      const OldPassowrd = Swal.getPopup().querySelector('#OldPassowrd').value
+      const NewPassword = Swal.getPopup().querySelector('#NewPassword').value
+      if (!OldPassowrd || !NewPassword) {
+        Swal.showValidationMessage(`Please enter login and password`)
+      }
+      return { OldPassowrd: OldPassowrd, NewPassword: NewPassword }
+    }
+  }).then((result) => {
+    ChangePassowrdForUser((data) => {
+      console.log("jere")
+      console.log(data)
+    }, data.id, result.value.OldPassowrd, result.value.NewPassword)
+  })
+
 }
