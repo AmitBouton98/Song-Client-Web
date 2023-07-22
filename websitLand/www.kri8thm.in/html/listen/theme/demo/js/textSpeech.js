a43141d56d6cb44cb7801b13d6013380ed5fe998
Csgroup17 <kh7292000@gmail.com>
2023-07-22 21:16:56 +0300

    add the speech to the website

---
+++ websitLand/www.kri8thm.in/html/listen/theme/demo/js/textSpeech.js
@@ -0,0 +1,21 @@
+let speaking = false;
+let utterance;
+
+function toggleSpeech() {
+  const text = $(".textToSpeak").text();
+  if (!speaking) {
+    utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = "en-US";
+    speechSynthesis.speak(utterance);
+    speaking = true;
+    // <i class="fa-solid fa-ear-deaf"></i>
+    //<i class="fa-solid fa-ear-listen"></i>
+    $(".theClassThatsThereNow");
+
+    $(".fa-ear-listen").addClass("fa-ear-deaf").removeClass("fa-ear-listen");
+  } else {
+    speechSynthesis.cancel();
+    speaking = false;
+    $(".fa-ear-deaf").addClass("fa-ear-listen").removeClass("fa-ear-deaf");
+  }
+}
