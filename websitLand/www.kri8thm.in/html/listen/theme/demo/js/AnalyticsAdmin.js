52c026ba9d5d4f1e74bee6893840d065b481c3f1
Csgroup17 <kh7292000@gmail.com>
2023-07-22 22:43:44 +0300

    add the data to the analytics UI

--- websitLand/www.kri8thm.in/html/listen/theme/demo/js/AnalyticsAdmin.js
+++ websitLand/www.kri8thm.in/html/listen/theme/demo/js/AnalyticsAdmin.js
@@ -10,9 +10,11 @@
 const user_info = JSON.parse(sessionStorage.getItem("User"));
 
 function LoadAdminPage() {
-  document.getElementById('UserFirstAndLastName').textContent = `${user_info.first} ${user_info.last}`
+  document.getElementById(
+    "UserFirstAndLastName"
+  ).textContent = `${user_info.first} ${user_info.last}`;
   LoadNumberOfUsersAndSongs();
-  LoadPlayedNumberForUserAndTopSongAndArtist()
+  LoadPlayedNumberForUserAndTopSongAndArtist();
 }
 function LoadNumberOfUsersAndSongs() {
   GetNumberOfUsers((data) => {
@@ -24,20 +26,29 @@ function LoadNumberOfUsersAndSongs() {
     document.getElementById("TotalNumberOfSongs").innerHTML = data;
   });
 }
-function LoadPlayedNumberForUserAndTopSongAndArtist(){
-  const Played = document.getElementById('TotalTimeForHearingSong')
-  GetNumberOfPlayedForUser((data)=>{
-    console.log(data)
-    Played.textContent = data
-  },user_info.id)
-  const Score = document.getElementById('GetScoreForUser')
-  GetScoreForUser((data)=>{
-    Score.textContent = data
-  },user_info.id)
-  GetTop1SongForUser((data)=>{
-    console.log(data)
-  },user_info.id)
-  GetTop1ArtistForUser((data)=>{
-    console.log(data)
-  },user_info.id)
+function LoadPlayedNumberForUserAndTopSongAndArtist() {
+  const Played = document.getElementById("TotalTimeForHearingSong");
+  GetNumberOfPlayedForUser((data) => {
+    console.log(data);
+    Played.textContent = data;
+  }, user_info.id);
+  const Score = document.getElementById("GetScoreForUser");
+  GetScoreForUser((data) => {
+    Score.textContent = data;
+  }, user_info.id);
+  GetTop1SongForUser((data) => {
+    $("#most-song-img").attr("src", data.urlLink);
+    $("#song-name").text(data.name);
+    $("#info-most-song").text(data.lyricLink);
+    // $("#artist-name").text(artist.artist.bio.summary);
+    console.log(data);
+  }, user_info.id);
+  GetTop1ArtistForUser((data) => {
+    $("#most-artist-img").attr("src", data.artistUrl);
+    $("#artist-name").text(data.artistName);
+    GetArtistInfo((artist) => {
+      $("#info-most-artist").html(artist.artist.bio.summary);
+    }, data.artistName);
+    console.log(data);
+  }, user_info.id);
 }
