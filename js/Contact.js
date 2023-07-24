ede922f8e5879481639525e74d0224fe13c4e1af
AmitBouton98 <118891774+AmitBouton98@users.noreply.github.com>
2023-07-24 12:32:35 +0300

    .

--- js/Contact.js
+++ js/Contact.js
@@ -1,11 +1,12 @@
-function SendMessageContactPage() {
-    $('#SendMessage').submit(SubmitMessage)
-    return false
 
+function SendMessageContactPage() {
+    $('#SendMessage').submit(SubmitMessage);
+    return false;
 }
 
-function SubmitMessage(e){
+function SubmitMessage(e) {
     e.preventDefault();
+
     $.ajax({
         type: 'POST',
         url: 'send_email.php', // The correct path to your server-side script
@@ -20,6 +21,6 @@ function SubmitMessage(e){
             alert('An error occurred while sending the email.');
         }
     });
-    return false
 
+    return false;
 }
--- js/send_email.php
+++ js/send_email.php
@@ -1,27 +1,48 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve form data
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    // Compose the email message
-    $to = "amitbouton@gmail.com"; // Replace with the recipient's email address
-    $subject = "New Message from Website";
-    $body = "Name: " . $name . "\n";
-    $body .= "Email: " . $email . "\n";
-    $body .= "Message: " . $message;
+    // Include PHPMailer and its dependencies
+    require 'path/to/PHPMailerAutoload.php';
 
-    // Set headers
-    $headers = "From: sender@example.com"; // Replace with the sender's email address
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer;
+
+    // Enable SMTP
+    $mail->isSMTP();
+
+    // Set the SMTP host and port
+    $mail->Host = 'smtp.elasticemail.com';
+    $mail->Port = 587;
+
+    // Set SMTP authentication
+    $mail->SMTPAuth = true;
+    $mail->Username = 'amit.khaled.airbnb@gmail.com'; // Replace with the sender's email address
+    $mail->Password = '6BA88EB97CC6AE035885DC0CD3A95BB30CC8'; // Replace with the sender's email password
+
+    // Set the sender and recipient
+    $mail->setFrom('amit.khaled.airbnb@gmail.com', 'Sender Name');
+    $mail->addAddress('amitbouton@gmail.com', 'Recipient Name'); // Replace with the recipient's email address and name
+
+    // Set the email subject and body
+    $mail->Subject = 'New Message from Website';
+    $mail->Body = "Name: $name\n";
+    $mail->Body .= "Email: $email\n";
+    $mail->Body .= "Message: $message";
 
     // Send the email
-    if (mail($to, $subject, $body, $headers)) {
+    if ($mail->send()) {
         // Return a success response to JavaScript
         echo json_encode(array('status' => 'success', 'message' => 'Email sent successfully.'));
     } else {
         // Return an error response to JavaScript
-        echo json_encode(array('status' => 'error', 'message' => 'Failed to send email.'));
+        echo json_encode(array('status' => 'error', 'message' => 'Failed to send email. Error: ' . $mail->ErrorInfo));
     }
 }
 ?>
